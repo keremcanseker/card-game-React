3804eef698b751aad49ace746e28685016c853ee
keremcanskr <kerem.4555@gmail.com>
2023-03-25 18:41:10 +0300

    add deploy workflow

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+import Card from './components/components'
+const cardImages = [
+  { "src": "/src/assets/helmet-1.png", matched: false },
+  { "src": "/src/assets/potion-1.png", matched: false },
+  { "src": "/src/assets/sword-1.png", matched: false },
+  { "src": "/src/assets/shield-1.png", matched: false },
+  { "src": "/src/assets/ring-1.png", matched: false },
+  { "src": "/src/assets/scroll-1.png", matched: false },
+
+]
+
+
+
+function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setchoiceOne] = useState(null)
+  const [choiceTwo, setchoiceTwo] = useState(null)
+  //shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0);
+  }
+  //handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setchoiceTwo(card) : setchoiceOne(card)
+  }
+
+  useEffect(() => {
+    if ((choiceTwo)) {
+      if ((choiceOne.src === choiceTwo.src)) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceTwo.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn();
+      } else {
+        console.log("not a match")
+        setTimeout(() => resetTurn()
+
+          , 1000);
+
+      }
+
+    }
+
+
+
+
+
+  }, [choiceTwo])
+
+  console.log(cards);
+
+  //reset turns
+  const resetTurn = () => {
+    setchoiceOne(null);
+    setchoiceTwo(null);
+    setTurns(prevTurns => prevTurns + 1);
+  }
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className='card-grid'>
+        {cards.map(card => (
+          <Card key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          >
+
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/components.jsx
@@ -0,0 +1,28 @@
+// import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import './components.css'
+
+export default class Card extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.props.handleChoice(this.props.card)
+
+    }
+
+
+    render() {
+        return (
+            <div className='card' >
+                <div className={this.props.flipped ? "flipped" : ""}>
+                    <img className='front' src={this.props.card.src} alt="card front" />
+                    <img className='back' src='/src/assets/cover.png' alt="card back"
+                        onClick={this.handleClick} />
+                </div>
+            </div>
+        )
+    }
+}
+
