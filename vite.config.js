e43a2e4840db62f0a56d8b1a43f9dad4896fd309
Keremcan Şeker <72311638+keremcanseker@users.noreply.github.com>
2023-04-19 18:41:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/card-game-React/'
+  base: '/card-game-react/'
 })
